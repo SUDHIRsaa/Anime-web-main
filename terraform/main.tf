868b2f5ebbd0cbd1a51a555340a4cb0d1c3d2da8
SUDHIRsaa <sudmaurya2611@gmail.com>
2026-01-17 14:46:03 +0530

    Restrict SSH to my IP

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,34 +1,50 @@
 resource "aws_security_group" "anime_sg" {
   name        = "anime-web-sg"
-  description = "Security group for Anime Web"
+  description = "Secure security group for Anime Web"
 
+  # HTTP access for users
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
-  }
-
-  ingress {
+    description = "Allow HTTP from public internet"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # SSH restricted to your IP only
+  ingress {
+    description = "Allow SSH only from admin IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["103.195.248.154/32"]
+  }
+
+  # Restrict outbound traffic to HTTPS only
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description = "Allow outbound HTTPS only"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "anime_web" {
-  ami                    = "ami-0f5ee92e2d63afc18"  
+  ami                    = "ami-0f5ee92e2d63afc18"
   instance_type          = "t2.micro"
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.anime_sg.id]
+
+  
+  root_block_device {
+    encrypted = true
+  }
+
+  # Enforce IMDSv2 (fixes AVD-AWS-0028)
+  metadata_options {
+    http_tokens = "required"
+  }
 
   tags = {
     Name = "Anime-Web-DevSecOps"
